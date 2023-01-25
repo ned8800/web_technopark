9c275dc652c0582182bae7504b4f1d455e527ab2
Timofey Chichikin <72801901+ned8800@users.noreply.github.com>
2023-01-25 17:00:08 +0300

    done lab4

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from app import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('hot', views.hot, name='hot'),
+    path('question/<int:question_id>', views.question, name='question'),
+    path('ask', views.ask, name='ask'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
+    path('settings', views.settings, name='settings'),
+    path('tag/<int:tag_id>', views.tag, name='tag'),
+]
